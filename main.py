fb117cfcdf226c2d961ac5fa5ee297e2fec5b28b
Mikhail Leonov <MikeLeonIt@yandex.ru>
2023-12-13 15:56:06 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,83 @@
+import encodings
+from design import *
+from PyQt6 import QtCore, QtGui, QtWidgets
+import pdf_reader, sys
+
+
+class Leonscene(QtWidgets.QGraphicsScene):
+    def __init__(self, parent=None):
+        super(Leonscene, self).__init__(parent)
+
+    def mousePressEvent(self, event):
+        super(Leonscene, self).mousePressEvent(event)
+        self.xRect = event.scenePos().x()
+        self.yRect = event.scenePos().y()
+
+    def mouseReleaseEvent(self, event):
+        super(Leonscene, self).mouseReleaseEvent(event)
+        add_rect = QtWidgets.QGraphicsScene.addRect(self, self.xRect, self.yRect, self.endX -
+                                                    self.xRect, self.endY - self.yRect)
+        add_rect.setBrush(QtGui.QColor(0, 0, 255, 0))  # Заливка
+        add_rect.setPen(QtCore.Qt.GlobalColor.red)  # Контур
+
+    def mouseMoveEvent(self, event):
+        super(Leonscene, self).mouseMoveEvent(event)
+        self.endX = event.scenePos().x()
+        self.endY = event.scenePos().y()
+
+
+class MyApp(QtWidgets.QDialog):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_Dialog()
+        self.ui.setupUi(self)
+
+        self.ui.btnBrowse.clicked.connect(self.open_file)
+        self.ui.nextButton.clicked.connect(self.next)
+        self.ui.previousButton.clicked.connect(self.previous)
+        self.result = []
+        self.count = 0
+
+    def show_page(self, count):
+        scene = Leonscene(self)
+        pixmap = QtGui.QPixmap(self.result[count])
+        item = QtWidgets.QGraphicsPixmapItem(pixmap)
+        scene.addItem(item)
+
+        self.ui.graphicsView.setScene(scene)
+
+    def open_file(self):
+        if self.result == [] and self.count == 0:
+            filename, _ = QtWidgets.QFileDialog.getOpenFileName(self,
+                                                                "Open File", ".",
+                                                                "PDF Files (*.pdf);;All Files (*)",
+                                                                encodings.normalize_encoding('utf-8'))
+            self.result = pdf_reader.convert_pdf(filename)
+            self.show_page(self.count)
+        else:
+            self.result = []
+            self.count = 0
+            self.open_file()
+
+    def next(self):
+        if self.count + 1 <= len(self.result) - 1:
+            self.count += 1
+            self.show_page(self.count)
+        else:
+            self.count = 0
+            self.show_page(self.count)
+
+    def previous(self):
+        if self.count - 1 >= 0:
+            self.count -= 1
+            self.show_page(self.count)
+        else:
+            self.count = len(self.result) - 1
+            self.show_page(self.count)
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    class_instance = MyApp()
+    class_instance.show()
+    sys.exit(app.exec())
---
+++ pdf_reader.py
@@ -0,0 +1,30 @@
+import fitz
+import os, shutil
+
+
+def convert_pdf(filename):
+    doc = fitz.open(filename)
+    result = []
+
+    path_name = "tmp"
+    if not os.path.exists(path_name):
+        os.makedirs(path_name)
+    else:
+        for filename in os.listdir(path_name):
+            file_path = os.path.join(path_name, filename)
+            try:
+                if os.path.isfile(file_path) or os.path.islink(file_path):
+                    os.unlink(file_path)
+                elif os.path.isdir(file_path):
+                    shutil.rmtree(file_path)
+            except Exception as e:
+                print('Failed to delete %s. Reason: %s' % (file_path, e))
+        #os.makedirs(path_name)
+
+    for i, page in enumerate(doc):
+        pix = page.get_pixmap()
+        name = f"tmp/page_{i+1}.png"
+        pix.save(name)
+        result.append(name)
+
+    return result
